1946c235a19fd87ec463332738da2bdec1db08e2
Darian Hughes <darianmhughes7@gmail.com>
2026-04-17 13:33:17 -0500

    some name adjustments to exo box variants and actually removed speed buff/nerf

--- Content/Items/Mounts/FlameCube.cs
+++ Content/Items/Mounts/FlameCube.cs
@@ -51,7 +51,7 @@ namespace Clamity.Content.Items.Mounts
             player.mount.SetMount(ModContent.MountType<BrimChairMount>(), player);
             player.buffTime[buffIndex] = 10;
             player.Clamity().FlyingChair = true;
-            player.Clamity().FlyingChairPower = 13;
+            //player.Clamity().FlyingChairPower = 13;
         }
     }
 }
